94c5a59e87d813b4a353e02ebc807d57d5b93fbd
preslymorales <davidmorales15102004@gmail.com>
2025-02-08 01:36:17 -0500

    direcciones

--- get-weather.sh
+++ get-weather.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Script ejecutado el: $(date)" >> /mnt/d/ubuntu/python/Clima-Toronto-App/output.log
-/home/iccd332/miniforge3/envs/iccd332/bin/python3 /mnt/d/ubuntu/python/Clima-Toronto-App/clima-toronto-hoy.py >> /mnt/d/ubuntu/python/Clima-Toronto-App/output.log 2>&1
+echo "Script ejecutado el: $(date)" >> /home/iccd332/Clima-Toronto-App/output.log
+/home/iccd332/miniforge3/envs/iccd332/bin/python3 /home/iccd332/Clima-Toronto-App/clima-toronto-hoy.py >> /home/iccd332/Clima-Toronto-App/output.log 2>&1
