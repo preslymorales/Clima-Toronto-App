e2c460ed4b140146690dadef15ee4d2c17c81c8e
preslymorales <davidmorales15102004@gmail.com>
2025-02-08 23:20:00 -0500

    correción

--- get-weather.sh
+++ get-weather.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Script ejecutado el: $(date)" >> /home/iccd332/CityWeather/output.log
-/home/iccd332/miniforge3/envs/iccd332/bin/python3 /home/iccd332/CityWeather/main.py >> /home/iccd332/CityWeather/output.log 2>&1
+echo "Script ejecutado el: $(date)" >> /home/iccd332/TorontoWeather/output.log
+/home/iccd332/miniforge3/envs/iccd332/bin/python3 /home/iccd332/TorontoWeather/main.py >> /home/iccd332/TorontoWeather/output.log 2>&1
